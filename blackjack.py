ac118207a8787195991e6b7db6b9c047d0b2ad38
henos1206 <henostsegai@gmail.com>
2023-01-07 01:34:04 +0000

    dealer and player have been issued their initial hand

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,9 @@
 import random
 
+from rich.console import Console
 from rich.prompt import Prompt
-from rich.style import Style
 
+console = Console()
 prompt = Prompt()
 
 def generate_deck()-> list:
@@ -16,6 +17,7 @@ def generate_deck()-> list:
     deck = [value+suit for suit in suits for value in values ]
     return deck
 
+
 def shuffle_deck(deck: list)-> list:
     """Shuffles the deck
 
@@ -28,12 +30,63 @@ def shuffle_deck(deck: list)-> list:
     random.shuffle(deck)
     return deck
 
-def begin_game():
-    prompt.ask("[magenta]Hello, welcome to the BlackJack table! \n Are you ready to play?[/magenta]", choices = ['yes','no'])
-    return
+
+def begin_game() -> str:
+    """ Asks the player if they would like to start the game
+    Returns:
+        string: Player's response
+    """
+    return prompt.ask("""
+    [magenta]------------ Welcome to the Python Casino ------------ 
+    \n    ------------- High Stakes Blackjack Table ------------- 
+    \n    ---------------- Are you ready to play? ---------------[/magenta]
+    """, choices = ['yes','no'])
+    
+
+def players_initial_draw(shuffled_deck: list) -> tuple:
+    """Draw 2 cards for the player and remove these cards from the deck
+
+    Args:
+        shuffled_deck (list): initial shuffled deck
+
+    Returns:
+        tuple: the player's hand and the rest of the deck
+    """
+    players_hand = []
+    random_draw = random.choices(shuffled_deck,k = 2)
+    players_hand.append(random_draw)
+    shuffled_deck.remove(random_draw)
+    return players_hand[0], shuffled_deck
+
+
+def dealers_initial_draw(shuffled_deck: list) -> tuple:
+    """Draw 2 cards for the dealer and remove these cards from the deck
+
+    Args:
+        shuffled_deck (list): 50 card deck
+
+    Returns:
+        tuple: dealer's hand and 48 card deck
+    """
+
+    dealers_hand = []
+    random_draw = random.choices(shuffled_deck,k = 2)
+    print(random_draw)
+    print(shuffled_deck)
+    dealers_hand.append(random_draw)
+    shuffled_deck.remove(random_draw)
+    return dealers_hand[0], shuffled_deck
 
 
 if __name__ == "__main__":
     deck = generate_deck()
     shuffled_deck = shuffle_deck(deck)
-    begin_game()
+    ready_to_play = begin_game()
+    if ready_to_play == "no":
+        console.print("    ----------------- Come again soon ----------------------", style = "magenta")
+    else:
+        initial_player_hand, deck = players_initial_draw(shuffled_deck)
+        initial_dealer_hand, deck = dealers_initial_draw(deck)
+        console.print(f" You have drawn the {initial_player_hand[0]} and the {initial_player_hand[1]}", style = "magenta")
+        console.print(f" The dealer has drawn the {initial_dealer_hand[0]} and the ???")
+
